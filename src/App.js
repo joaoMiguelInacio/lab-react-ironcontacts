a18e3cdf06fa997dca5a1ae62d652ed676c85dc2
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-19 18:12:15 +0100

    Styling with Material UI phase 1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import "./App.css";
 import Contacts from "./contacts.json";
 import ContactItem from "./components/ContactItem";
 
-import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
+
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
 
 let contactsData = [
   Contacts[0],
@@ -56,22 +61,20 @@ function App() {
 
 
   return (
-    <div className='page'>
-      <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button onClick={() => handleSortByName()}>Sort by name</Button>
-        <Button onClick={() => handleSortByPopularity()}>Sort by popularity</Button>
-        <Button onClick={() => handleAdd()}>Add Random Contact</Button>
-      </ButtonGroup>
+    <div>
+      <Fab className='fabButton' variant="extended" color="primary" aria-label="add" onClick={() => handleAdd()}>
+        <AddIcon />
+        Add Random Contact
+      </Fab>
       <div className="contacts-list">
         <div className="contact">
           <table className='table'>
             <tr className='table-body'>
                 <th className ='table-head'>Picture</th>
-                <th className ='table-head'>Name</th>
-                <th className ='table-head'>Popularity</th>
+                <th className ='table-head'><Button fullWidth fullHeight endIcon={<ArrowDropDownIcon />} variant="text"  /*contained, outlined*/ color="inherit" onClick={() => handleSortByName()}>Name</Button></th>
+                <th className ='table-head'><Button fullWidth fullHeight endIcon={<ArrowDropDownIcon />} variant="text" color="inherit" /*, success, primary, secondary, info, inherit, error, warning*/ onClick={() => handleSortByPopularity()}>Popularity</Button></th>
                 <th className ='table-head'>Oscar</th>
                 <th className ='table-head'>Emmy</th>
-                <th className ='table-head'>Delete</th>
             </tr>
             {displayContacts()}
           </table>
--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,4 +1,13 @@
-import Button from '@mui/material/Button';
+
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import Checkbox from '@mui/material/Checkbox';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
 
 export default function ContactItem({ name, pictureUrl, popularity, wonOscar, wonEmmy, handleDelete}) {
     if(false){
@@ -8,12 +17,52 @@ export default function ContactItem({ name, pictureUrl, popularity, wonOscar, wo
     } else {
       return (
             <tr className='table-body'>
-                <td><img style={{width:100, height:150 }} src={pictureUrl} alt="no-picture" /></td>
-                <td>{name}</td>
+                <td>
+                  <Avatar className='center'
+                    variant="rounded"
+                    alt={name}
+                    src={pictureUrl}
+                    sx={{ width: 50, height: 75 }}
+                  />
+                </td>
+                <td>{name} <IconButton onClick={() => handleDelete(name)}><DeleteIcon  color="error"/></IconButton></td>
                 <td>{popularity}</td>
-                <td>{wonOscar ? <p>🏆</p> : <p>🙈</p> }</td>
-                <td>{wonEmmy ? <p>🏆</p> : <p>🙈</p>}</td>
-                <td><Button onClick={() => handleDelete(name)}>Delete</Button></td>
+                <td>
+                  {
+                    wonOscar ?  
+                    <p>
+                      <Checkbox 
+                        icon={<EmojiEventsOutlinedIcon fontSize="large"/>} 
+                        checkedIcon={<EmojiEventsIcon fontSize="large" color="warning"/>} 
+                        defaultChecked 
+                      />
+                    </p> 
+                    : <p>
+                        <Checkbox  
+                          icon={<EmojiEventsOutlinedIcon fontSize="large"/>}
+                          checkedIcon={<EmojiEventsIcon fontSize="large" color="warning"/>} 
+                        />
+                      </p> 
+                  }
+                </td>
+                <td>
+                  {
+                    wonEmmy ?  
+                    <p>
+                      <Checkbox 
+                        icon={<EmojiEventsOutlinedIcon fontSize="large"/>} 
+                        checkedIcon={<EmojiEventsIcon fontSize="large" color="warning"/>} 
+                        defaultChecked 
+                      />
+                    </p> 
+                    : <p>
+                        <Checkbox  
+                          icon={<EmojiEventsOutlinedIcon fontSize="large"/>}
+                          checkedIcon={<EmojiEventsIcon fontSize="large" color="warning"/>} 
+                        />
+                      </p> 
+                  }
+                </td>
             </tr>
       )
     } 
